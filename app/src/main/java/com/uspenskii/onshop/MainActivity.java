eaf650d49f8a345d2548f00c30a00f960670c462
uspenskii <andrei.uspenskii@gmail.com>
2016-01-11 17:04:27 +0300

    Initial commit

---
+++ app/src/main/java/com/uspenskii/onshop/MainActivity.java
@@ -0,0 +1,126 @@
+package com.uspenskii.onshop;
+
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.uspenskii.onshop.fragments.FragmentAbout;
+import com.uspenskii.onshop.fragments.FragmentHome;
+import com.uspenskii.onshop.fragments.FragmentLike;
+import com.uspenskii.onshop.fragments.FragmentSettings;
+import com.uspenskii.onshop.fragments.FragmentShop;
+import com.uspenskii.onshop.fragments.FragmentUser;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    FragmentAbout fabout;
+    FragmentHome fhome;
+    FragmentLike flike;
+    FragmentSettings fsettings;
+    FragmentShop fshop;
+    FragmentUser fuser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setItemIconTintList(null);
+
+        fabout = new FragmentAbout();
+        fhome = new FragmentHome();
+        flike = new FragmentLike();
+        fsettings = new FragmentSettings();
+        fshop = new FragmentShop();
+        fuser = new FragmentUser();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        FragmentTransaction ftrans = getFragmentManager().beginTransaction();
+
+        if (id == R.id.nav_home) {
+            ftrans.replace(R.id.container, fhome);
+        } else if (id == R.id.nav_shop) {
+            ftrans.replace(R.id.container, fshop);
+        } else if (id == R.id.nav_like) {
+            ftrans.replace(R.id.container, flike);
+        } else if (id == R.id.nav_settings) {
+            ftrans.replace(R.id.container, fsettings);
+        } else if (id == R.id.nav_user) {
+            ftrans.replace(R.id.container, fuser);
+        } else if (id == R.id.nav_about) {
+            ftrans.replace(R.id.container, fabout);
+        }ftrans.commit();
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/uspenskii/onshop/Pager/ViewPagerSampleActivity.java
@@ -0,0 +1,46 @@
+package com.uspenskii.onshop.Pager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.uspenskii.onshop.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerSampleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        List<View> pages = new ArrayList<View>();
+
+        View page = inflater.inflate(R.layout.fragment_home, null);
+        TextView textView = (TextView) page.findViewById(R.id.text_view);
+        textView.setText("Страница 1");
+        pages.add(page);
+
+        page = inflater.inflate(R.layout.fragment_home, null);
+        textView = (TextView) page.findViewById(R.id.text_view);
+        textView.setText("Страница 2");
+        pages.add(page);
+
+        page = inflater.inflate(R.layout.fragment_home, null);
+        textView = (TextView) page.findViewById(R.id.text_view);
+        textView.setText("Страница 3");
+        pages.add(page);
+
+        SamplePagerAdapter pagerAdapter = new SamplePagerAdapter(pages);
+        ViewPager viewPager = new ViewPager(this);
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.setCurrentItem(1);
+
+        setContentView(viewPager);
+    }
+}
