3d8738251469edc23c833c98dfcfaf8a583f5266
mac12841 <san.delli@yandex.ru>
2016-01-12 00:42:55 +0300

    hello)

--- app/src/main/java/com/uspenskii/onshop/MainActivity.java
+++ app/src/main/java/com/uspenskii/onshop/MainActivity.java
@@ -1,13 +1,10 @@
 package com.uspenskii.onshop;
 
-import android.app.Fragment;
 import android.app.FragmentTransaction;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -15,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.uspenskii.onshop.fragments.FragmentAbout;
 import com.uspenskii.onshop.fragments.FragmentHome;
@@ -31,7 +29,7 @@ public class MainActivity extends AppCompatActivity
     FragmentSettings fsettings;
     FragmentShop fshop;
     FragmentUser fuser;
-
+    //hello world!
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
